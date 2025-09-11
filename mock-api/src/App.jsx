04a1201b4c7dd78e5a07e4d337aa20fc6b002806
Minh2005 <nguyenhoangminhddd@gmail.com>
2025-09-11 20:38:40 +0700

    mock
    push

--- mock-api/src/App.jsx
+++ mock-api/src/App.jsx
@@ -7,7 +7,7 @@ function App() {
       <button className="btn btn-primary">Primary</button>
       <button className="btn btn-secondary">Secondary</button>
       <button className="btn btn-accent">Accent</button>
-      <button className="btn btn-info">Infhazardo</button>
+      <button className="btn btn-info">Info</button>
       <button className="btn btn-success">Success</button>
       <button className="btn btn-warning">Warning</button>
       <button className="btn btn-error">Error</button>
