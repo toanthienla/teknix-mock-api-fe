c06e1f54bb30f244a70bb852dd6de19b923e91c4
Minh2005 <nguyenhoangminhddd@gmail.com>
2025-09-15 10:42:55 +0700

    Update App.jsx

--- mock-api/src/App.jsx
+++ mock-api/src/App.jsx
@@ -6,7 +6,7 @@ function App() {
       <button className="btn btn-neutral">Neutral</button>
       <button className="btn btn-primary">Primary</button>
       <button className="btn btn-secondary">Secondary</button>
-      <button className="btn btn-accent">Accent</button>
+      <button className="btn btn-accent">Accents</button>
       <button className="btn btn-info">Infos</button>
       <button className="btn btn-success">Success</button>
       <button className="btn btn-warning">Warning</button>
