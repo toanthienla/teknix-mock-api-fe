aa30159b39a7cfa88727dcb60ad858821d292bdb
Minh2005 <nguyenhoangminhddd@gmail.com>
2025-09-17 16:53:35 +0700

    as
    as

--- mock-api/src/App.jsx
+++ mock-api/src/App.jsx
@@ -1,15 +1,16 @@
 import { Routes, Route, Navigate } from "react-router-dom";
 import DashboardPage from "./pages/DashboardPage";
-import MainContentSection from "./components/MainContentSection";
-
+import EndpointDetail from "./pages/Endpointdetail";
 
 export default function App() {
   return (
     <Routes>
       <Route path="/" element={<Navigate to="/dashboard" replace />} />
       <Route path="/dashboard" element={<DashboardPage />} />
-      <Route path="/dashboard/:projectId" element={<MainContentSection />} />
-    
+      <Route
+        path="/dashboard/:projectId/endpoint/:endpoint_id"
+        element={<EndpointDetail />}
+      />
     </Routes>
   );
 }
