b5b5517895b06d23fccafda267cc4ec4df3f4b65
jaPhuc <phuctlg.ce190872@gmai.com>
2025-10-31 11:38:42 +0700

    Remove centrifugo

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,4 +1,4 @@
 const API_ROOT = import.meta.env.VITE_API_ROOT;
-const API_WS_ROOT = import.meta.env.VITE_API_WS_ROOT;
+// const API_WS_ROOT = import.meta.env.VITE_API_WS_ROOT;
 
-export { API_ROOT, API_WS_ROOT };
+export { API_ROOT };
